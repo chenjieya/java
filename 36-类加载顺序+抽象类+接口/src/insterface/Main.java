1193fcdc0a0261a4f65c8a53bc1dc6804eaacf1e
chenjie <18822126754@163.com>
2023-06-27 20:48:31 +0800

    【chenjie】接口

---
+++ 36-类加载顺序+抽象类+接口/src/insterface/Main.java
@@ -0,0 +1,23 @@
+package insterface;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * 接口
+         *
+         * 抽象类抽象到极致就是接口（里面的内容必须全部都是抽象的）
+         *
+         * 接口只能包含：
+         *  公共静态不可变的属性 public static final
+         *  公共抽象方法 public abstract
+         *
+         * 不能包含块，构造函数
+         * 没有构造函数也不能创建对象
+         *
+         * 使用方法：
+         *  只能通过子类多实现（implements）来做事情
+         *      public class A implements B,C,D {}
+         *
+         */
+    }
+}
---
+++ 36-类加载顺序+抽象类+接口/src/insterface/TestInterface.java
@@ -0,0 +1,14 @@
+package insterface;
+
+public interface TestInterface {
+    public static final String name = "test";
+
+    // 可以直接省略
+    String uname = "test";
+
+    public abstract void test();
+
+    // 可以省略
+    public void tetsa();
+
+}
