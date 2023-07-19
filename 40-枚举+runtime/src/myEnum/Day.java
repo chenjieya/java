869798419931da5c6d9ddb6e1b76a4a5ac5dfa0e
chenjie <18822126754@163.com>
2023-07-19 19:41:25 +0800

    【chenjie】枚举

--- 40-枚举+runtime/src/myEnum/Day.java
+++ 40-枚举+runtime/src/myEnum/Day.java
@@ -1,5 +1,17 @@
 package myEnum;
 
 public enum Day {
-    Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
+    Monday("星期一", 1),Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday;
+
+    private String name;
+    private int index;
+
+    private Day(){};
+    private Day(String name, int index) {
+        this.index = index;
+    };
+
+    public String getName() {
+        return this.name;
+    }
 }
