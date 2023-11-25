206467e822f6065f70547989dabc93b179fa9886
alvis <alvis.goodday@gmail.com>
2023-11-25 17:43:40 +0800

    feat(FilterChain): 多个Filter执行顺序

---
+++ FilterChain/src/filter/FilterOne.java
@@ -0,0 +1,20 @@
+package filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FilterOne extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        System.out.println("FilterOne-start");
+
+        chain.doFilter(request, response);;
+        System.out.println("FilterOne-end");
+    }
+}
