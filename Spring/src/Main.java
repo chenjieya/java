cd50d11ff4607a44c0e7d7c1684868a8ce159b92
alvis <alvis.goodday@gmail.com>
2023-12-06 19:30:13 +0800

    feat(Spring-IOC): 拆分配置文件

--- Spring/src/Main.java
+++ Spring/src/Main.java
@@ -7,9 +7,23 @@ public class Main {
 
 
         // 创建对象
-        BeanFactory factory = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Student stu = (Student)factory.getBean("student");
-        System.out.println(stu);
+//        BeanFactory factory = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        Student stu = (Student)factory.getBean("student");
+//        System.out.println(stu);
+
+
+        // 写法一：重载
+        //BeanFactory factory = new ClassPathXmlApplicationContext("applicationContext.xml", "applicationContext_dao.xml","applicationContext_controller.xml","applicationContext_service.xml");
+
+
+        // 写法二：数组
+        // BeanFactory factory = new ClassPathXmlApplicationContext(new String[]{"applicationContext.xml", "applicationContext_dao.xml","applicationContext_controller.xml","applicationContext_service.xml"});
+
+
+        // 写法三：通配符
+        //BeanFactory factory = new ClassPathXmlApplicationContext("applicationContext*.xml");
+
+        // 写法四：在基础xml配置文件中，直接引入其他文件
 
     }
 }
