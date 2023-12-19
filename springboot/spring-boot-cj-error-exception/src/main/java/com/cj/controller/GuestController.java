7f65ef774ee75ed2637185e253b2196d15d442cd
chenjie <18822126754@163.com>
2023-12-19 16:43:21 +0800

    feat(spring-boot): 数据校验

--- springboot/spring-boot-cj-error-exception/src/main/java/com/cj/controller/GuestController.java
+++ springboot/spring-boot-cj-error-exception/src/main/java/com/cj/controller/GuestController.java
@@ -2,15 +2,29 @@ package com.cj.controller;
 
 import com.cj.bean.Guest;
 import jakarta.validation.Valid;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class GuestController {
 
     @PostMapping("/guest")
-    public String add(@Valid Guest guest) {
-        return "add guest success";
+    public String add(@Valid Guest guest, BindingResult result) {
+        StringBuffer sringbuffer = new StringBuffer();
+        if(result.getErrorCount() > 0) {
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            for (FieldError fieldError : fieldErrors) {
+                sringbuffer.append(fieldError.getField());
+                sringbuffer.append(":");
+                sringbuffer.append(fieldError.getDefaultMessage());
+                sringbuffer.append("\n");
+            }
+        }
+        return sringbuffer.toString();
     }
 
 
